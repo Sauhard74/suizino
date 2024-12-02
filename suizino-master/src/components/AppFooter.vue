329b93e96f5c19e3899a01ef250e464d715c016c
hardikjumnani <sauhard74best@gmail.com>
2024-12-02 11:35:56 +0530

    suizino web commit

---
+++ suizino-master/src/components/AppFooter.vue
@@ -0,0 +1,15 @@
+
+<script setup>
+</script>
+<template>
+
+  <footer id="footer" class="text-center">
+
+
+    <p class="text-xs mt-2 pb-6">
+      This is project is built during Unfold24: <a href="https://github.com/Sauhard74" target="_blank" rel="nofollow">https://github.com/Sauhard74</a>
+    </p>
+
+  </footer>
+
+</template>
---
+++ suizino-master/src/helpers/constants.js
@@ -0,0 +1,8 @@
+export const suiRpcUrl = 'https://fullnode.devnet.sui.io:443'
+export const casinoAddress ='0x9c53d2454eb7a1c6799bea76691ce73ee6132831';
+export const moduleAddress = '0x9dadc949e5acebf22941e10a5241bca40772b571';
+export const moduleName = 'Suizino_core';
+export const localStorageKeys = {
+    address: 'user_sui_address',
+    walletProvider: 'wallet_provider'
+}
